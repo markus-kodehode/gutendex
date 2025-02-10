012680ba7871f8c6b001ebf2d202d0581607fdf3
markus-kodehode <markus2.kodehode@gmail.com>
2025-02-10 09:34:20 +0100

    Removed HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 // Router components
@@ -10,29 +10,32 @@ import CategoryPage from "./pages/CategoryPage.jsx";
 import SearchResults from "./components/SearchResults.jsx";
 import FavoritesPage from "./pages/FavoritesPage.jsx";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: (
-      <h1>Error, something went wrong and an oopsie has been made!</h1>
-    ),
-    children: [
-      { index: true, element: <SearchResults /> },
-      { path: "/book/:bookId", element: <BookPage /> },
-      { path: "/category/:category", element: <CategoryPage /> },
-      { path: "/favorites", element: <FavoritesPage /> },
-    ],
-  },
-  {
-    path: "*",
-    element: (
-      <div className="error-page">
-        <h1>404 - Page Not Found</h1>
-      </div>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: (
+        <h1>Error, something went wrong and an oopsie has been made!</h1>
+      ),
+      children: [
+        { index: true, element: <SearchResults /> },
+        { path: "book/:bookId", element: <BookPage /> },
+        { path: "category/:category", element: <CategoryPage /> },
+        { path: "favorites", element: <FavoritesPage /> },
+      ],
+    },
+    {
+      path: "*",
+      element: (
+        <div className="error-page">
+          <h1>404 - Page Not Found</h1>
+        </div>
+      ),
+    },
+  ],
+  { basename: "/gutendex" }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
